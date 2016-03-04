3cb7b72210ae658a4d641aa693a65714fae50cde
Omar Taylor <omtaylor@iastate.edu>
2016-03-04 14:31:15 -0600

    Clean Up

--- project1/Database.java
+++ project1/Database.java
@@ -6,225 +6,237 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
- * 
  * @author Omar Taylor This class scans through a data set of 100000 ratings by
  *         943 users on 1682 items and sorts them into a database.
- * 
+ *         <p>
  *         Known Issues:
  */
 public class Database {
+    // ItemID is the first index and UserID is the second
+    public int[][] matrix;
+    String[][] staticMatrix = new String[5][5];
+    private int M; // number of rows
+    private int N; // number of columns
 
-	String[][] staticDb = new String[5][5]; // ItemID is the first index and
-											// UserID is the second
+    /**
+     * Create empty database
+     *
+     * @throws FileNotFoundException
+     */
+    public Database() throws FileNotFoundException {
+        separateCols();
+        removeTimeStamp();
+    }
 
-	public int[][] matrix;
-	private int[] movies;
-	private int[] users;
+    /**
+     * Create M-by-N matrix of 0's
+     */
+    public Database(int M, int N) {
+        this.M = M;
+        this.N = N;
+        matrix = new int[M][N];
+    }
 
-	private int M; // number of rows
-	private int N; // number of columns
+    /**
+     * Initialize database based on previous database
+     *
+     * @param matrix Existing matrix to be scanned in.
+     */
+    public Database(int[][] matrix) {
+        M = matrix.length;
+        N = matrix[0].length;
+        this.matrix = new int[M][N];
+        for (int i = 0; i < M; i++)
+            System.arraycopy(matrix[i], 0, this.matrix[i], 0, N);
+    }
 
-	/**
-	 * Create empty database
-	 * 
-	 * @throws FileNotFoundException
-	 */
-	public Database() throws FileNotFoundException {
-		separateCols();
-	}
+    /**
+     * Searches through given file and sorts the data in a dynamically
+     * implemented 2D array (matrix) called the database.
+     *
+     * @throws FileNotFoundException
+     */
+    private void buildDynamic() throws FileNotFoundException {
+        setUp();
+        Scanner sc = new Scanner(new File("u_parsed.txt"));
 
-	/**
-	 * Create M-by-N matrix of 0's
-	 * 
-	 * @throws FileNotFoundException
-	 */
-	public Database(int M, int N) {
-		this.M = M;
-		this.N = N;
-		matrix = new int[M][N];
-	}
+        while (sc.hasNextInt()) {
+            matrix[sc.nextInt()][sc.nextInt()] = sc.nextInt();
+        }
+        sc.close();
+    }
 
-	/**
-	 * Initialize database based on previous database
-	 * 
-	 * @param data
-	 */
-	public Database(int[][] matrix) {
-		M = matrix.length;
-		N = matrix[0].length;
-		this.matrix = new int[M][N];
-		for (int i = 0; i < M; i++)
-			for (int j = 0; j < N; j++)
-				this.matrix[i][j] = matrix[i][j];
-	}
+    /**
+     * Searches through given file and sorts the data in a statically
+     * implemented 2D array (matrix) called the database.
+     *
+     * @throws FileNotFoundException
+     */
+    private void buildStatic() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("u_parsed.txt"));
 
-	/**
-	 * Searches through given file and sorts the data in a dynamically
-	 * implemented 2D array (matrix) called the database.
-	 * 
-	 * @throws FileNotFoundException
-	 */
-	public void buildDynamic() throws FileNotFoundException {
-		setUp();
-		Scanner sc = new Scanner(new File("u_parsed.txt"));
+        int userID;
+        int itemID;
+        int rating;
 
-		while (sc.hasNextInt()) {
-			matrix[sc.nextInt()][sc.nextInt()] = sc.nextInt();
-		}
-		sc.close();
-	}
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int l = 0;
+        int m = 0;
+        while (sc.hasNextInt()) {
+            userID = sc.nextInt(); // Get userID
+            itemID = sc.nextInt(); // Get itemID
+            rating = sc.nextInt(); // Get rating
 
-	/**
-	 * Searches through given file and sorts the data in a statically
-	 * implemented 2D array (matrix) called the database.
-	 *
-	 * 
-	 * @param data
-	 * @throws FileNotFoundException
-	 */
-	public void buildStatic() throws FileNotFoundException {
-		Scanner sc = new Scanner(new File("u_parsed.txt"));
+            // Stores the database input in the following format: [UserID ItemID
+            // Rating].
+            // String consists of numbers for easy processing
+            if (itemID == 242 && i < 5) {
+                staticMatrix[0][i++] = userID + " " + itemID + " " + rating;
+            }
+            if (itemID == 302 && j < 5) {
+                staticMatrix[1][j++] = userID + " " + itemID + " " + rating;
+            }
+            if (itemID == 377 && k < 5) {
+                staticMatrix[2][k++] = userID + " " + itemID + " " + rating;
+            }
+            if (itemID == 51 && l < 5) {
+                staticMatrix[3][l++] = userID + " " + itemID + " " + rating;
+            }
+            if (itemID == 346 && m < 5) {
+                staticMatrix[4][m++] = userID + " " + itemID + " " + rating;
+            }
+        }
+        sc.close();
+    }
 
-		int userID = 0;
-		int itemID = 0;
-		int rating = 0;
+    /**
+     * This function separates each column into their own respective files
+     *
+     * @throws FileNotFoundException
+     */
+    private void separateCols() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("u.txt"));
+        PrintWriter users = new PrintWriter("u_usersonly.txt");
+        PrintWriter movies = new PrintWriter("u_moviesonly.txt");
+        PrintWriter ratings = new PrintWriter("u_ratingsonly.txt");
+        while (sc.hasNextInt()) {
+            users.println(sc.nextInt());
+            movies.println(sc.nextInt());
+            ratings.println(sc.nextInt());
+            sc.nextInt(); // throw away time stamp
+        }
+        users.close();
+        movies.close();
+        ratings.close();
+        sc.close();
+    }
 
-		/*
-		 * SOLVED: UserIDs are hard-coded i, j, k, l, and m. ItemIDs are
-		 * hard-coded as 0, 1, 2, 3, and 4. Have not yet found a way to process
-		 * both counts automatically rather than manually
-		 */
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		int l = 0;
-		int m = 0;
-		while (sc.hasNextInt()) {
-			userID = sc.nextInt(); // Get userID
-			itemID = sc.nextInt(); // Get itemID
-			rating = sc.nextInt(); // Get rating
+    /**
+     * This function scans in u.txt and writes to a new file without the time
+     * stamp
+     *
+     * @throws FileNotFoundException
+     */
+    private void removeTimeStamp() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("u.txt"));
+        PrintWriter pw = new PrintWriter("u_parsed.txt");
 
-			// Stores the database input in the following format: [UserID ItemID
-			// Rating].
-			// String consists of numbers for easy processing
-			if (itemID == 242 && i < 5) {
-				staticDb[0][i++] = userID + " " + itemID + " " + rating;
-			} else if (itemID == 302 && j < 5) {
-				staticDb[1][j++] = userID + " " + itemID + " " + rating;
-			} else if (itemID == 377 && k < 5) {
-				staticDb[2][k++] = userID + " " + itemID + " " + rating;
-			} else if (itemID == 51 && l < 5) {
-				staticDb[3][l++] = userID + " " + itemID + " " + rating;
-			} else if (itemID == 346 && m < 5) {
-				staticDb[4][m++] = userID + " " + itemID + " " + rating;
-			} else {
-				continue;
-			}
-		}
-		sc.close();
-	}
+        while (sc.hasNextInt()) {
+            pw.println(sc.nextInt() + " " + sc.nextInt() + " " + sc.nextInt());
+            sc.nextInt(); // throw away time stamp
+        }
+        pw.close();
+        sc.close();
+    }
 
-	/**
-	 * This function separates each column into their own respective files
-	 * 
-	 * @param data
-	 * @throws FileNotFoundException
-	 */
-	private void separateCols() throws FileNotFoundException {
-		Scanner sc = new Scanner(new File("u.txt"));
-		PrintWriter users = new PrintWriter("u_usersonly.txt");
-		PrintWriter movies = new PrintWriter("u_moviesonly.txt");
-		PrintWriter ratings = new PrintWriter("u_ratingsonly.txt");
-		while (sc.hasNextInt()) {
-			users.write(sc.nextInt() + "\n");
-			movies.write(sc.nextInt() + "\n");
-			ratings.write(sc.nextInt() + "\n");
-			sc.nextInt(); // throw away time stamp
-		}
-		users.close();
-		movies.close();
-		ratings.close();
-		sc.close();
-	}
+    /**
+     * This function creates the row and column headers for the matrix. Does this by removing duplicate entries in the scanned in file.
+     * Called twice to return array for row and column
+     *
+     * @param in
+     * @param out
+     * @param l
+     * @return array of headers to be used for the row and column of matrix
+     * @throws FileNotFoundException
+     */
+    private int[] createHeaders(File in, String out, int l) throws FileNotFoundException {
+        Scanner sc = new Scanner(in);
+        PrintWriter pw = new PrintWriter(out);
+        int[] counters = new int[l];
+        int length = 0;
 
-	private int[] removeDuplicates(File in, String out, int l) throws FileNotFoundException {
-		Scanner sc = new Scanner(in);
-		PrintWriter pw = new PrintWriter(out);
-		int[] counters = new int[l];
-		int length = 0;
+        while (sc.hasNextInt()) {
+            int temp = sc.nextInt();
+            counters[temp] = 1;
+        }
 
-		while (sc.hasNextInt()) {
-			int temp = sc.nextInt();
-			counters[temp] = 1;
-		}
+        for (int counter : counters) {
+            if (counter == 1) {
+                length++;
+            }
+        }
 
-		for (int i = 0; i < counters.length; i++) {
-			if (counters[i] == 1) {
-				length++;
-			}
-		}
+        sc.close();
+        int[] array = new int[length + 1];
 
-		sc.close();
-		int[] array = new int[length + 1];
+        for (length = 0; length <= array.length; length++) {
+            if (counters[length] == 0) {
+                continue;
+            }
+            pw.write(length + "\n");
+            array[length] = length;
+        }
+        pw.close();
+        return array;
+    }
 
-		for (length = 0; length <= array.length; length++) {
-			if (counters[length] == 0) {
-				continue;
-			}
-			pw.write(length + "\n");
-			array[length] = length;
-		}
-		pw.close();
-		return array;
-	}
+    /**
+     * Creates a matrix of movies (columns), users (rows), and ratings (data).
+     *
+     * @throws FileNotFoundException
+     */
+    private void setUp() throws FileNotFoundException {
+        Scanner sc;
+        int mLength = 0;
+        int uLength = 0;
+        File f1 = new File("u_moviesonly.txt");
+        File f2 = new File("u_usersonly.txt");
 
-	/**
-	 * This function scans in u.txt and writes to a new file without the time
-	 * stamp
-	 * 
-	 * @param data
-	 *            The file
-	 * @throws FileNotFoundException
-	 */
-	public void removeTimeStamp(String data) throws FileNotFoundException {
-		Scanner sc = new Scanner(new File(data));
-		PrintWriter pw = new PrintWriter("u_parsed.txt");
+        for (sc = new Scanner(f1); sc.hasNextInt(); sc.nextInt())
+            mLength++;
+        sc.close();
+        for (sc = new Scanner(f2); sc.hasNextInt(); sc.nextInt())
+            uLength++;
+        sc.close();
+        int[] movies = createHeaders(f1, "u_movies_nodup.txt", mLength);
+        int[] users = createHeaders(f2, "u_users_nodup.txt", uLength);
+        matrix = new int[users.length][movies.length];
 
-		while (sc.hasNextInt()) {
-			pw.write(sc.nextInt() + " " + sc.nextInt() + " " + sc.nextInt() + "\n");
-			sc.nextInt(); // throw away time stamp
-		}
-		pw.close();
-		sc.close();
-	}
+        for (mLength = 0; mLength < matrix[0].length; mLength++)
+            matrix[0][mLength] = movies[mLength];
+        for (uLength = 0; uLength < matrix.length; uLength++)
+            matrix[uLength][0] = users[uLength];
+    }
 
-	/**
-	 * Creates a matrix of movies (columns), users (rows), and ratings (data).
-	 * 
-	 * @param data
-	 * @param array
-	 * @throws FileNotFoundException
-	 */
-	private void setUp() throws FileNotFoundException {
-		Scanner sc;
-		int mLength = 0;
-		int uLength = 0;
-		File f1 = new File("u_moviesonly.txt");
-		File f2 = new File("u_usersonly.txt");
+    /**
+     * Generates and prints database to file. Must call buildDynamic (or buildStatic) to work.
+     *
+     * @throws FileNotFoundException
+     */
+    public void generateMatrix() throws FileNotFoundException {
+        buildDynamic();
+        PrintWriter pw = new PrintWriter("result.txt");
 
-		for (sc = new Scanner(f1); sc.hasNextInt(); sc.nextInt())
-			mLength++;
-		sc.close();
-		for (sc = new Scanner(f2); sc.hasNextInt(); sc.nextInt())
-			uLength++;
-		sc.close();
-		this.movies = removeDuplicates(f1, "u_movies_nodup.txt", mLength);
-		this.users = removeDuplicates(f2, "u_users_nodup.txt", uLength);
-		matrix = new int[this.users.length][this.movies.length];
-
-		for (mLength = 0; mLength < matrix[0].length; mLength++)
-			matrix[0][mLength] = this.movies[mLength];
-		for (uLength = 0; uLength < matrix.length; uLength++)
-			matrix[uLength][0] = this.users[uLength];
-	}
+        // iterate over the first dimension
+        for (int[] aMatrix : matrix) {
+            // iterate over the second dimension
+            for (int j = 0; j < aMatrix.length; j++)
+                if (j != aMatrix.length - 1) pw.print(aMatrix[j] + ", ");
+                else pw.print(aMatrix[j]); // remove the last separator
+            pw.println(); // add a line break.
+        }
+        pw.close();
+    }
 }
--- project1/Implementation.java
+++ project1/Implementation.java
@@ -1,31 +1,17 @@
 package project1;
 
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 
+/**
+ *
+ * @author Omar Taylor
+ *
+ * This class implements the project.
+ */
 public class Implementation {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Database d = new Database();
-		d.buildDynamic();
-		String separator = ", ";
-		StringBuffer result = new StringBuffer();
-		PrintWriter pw = new PrintWriter("result.txt");
-
-		// iterate over the first dimension
-		for (int i = 0; i < d.matrix.length; i++) {
-			// iterate over the second dimension
-			for (int j = 0; j < d.matrix[i].length; j++) {
-				result.append(d.matrix[i][j]);
-				result.append(separator);
-			}
-			// remove the last separator
-			result.setLength(result.length() - separator.length());
-			// add a line break.
-			result.append("\n");
-		}
-
-		pw.write(result.toString());
-		pw.close();
+		d.generateMatrix();
 	}
 }
